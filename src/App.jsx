5e49f2e4704dfec961a700acf7201bf7a07d55f6
Riltter <riltterkennedy@gmail.com>
2023-09-04 15:18:22 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Register from './components/pages/Register';
 import Profile from './components/pages/Profile';
 import Feed from './components/pages/Feed';
-import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import './App.css'
 
@@ -11,9 +11,15 @@ const App = () => {
 
   return (
     <>
+
       <Router>
+        <ul>
+          <Link to="/">Register</Link>
+          <Link to="/feed">Feed</Link>
+          <Link to="/profile">Profile</Link>
+        </ul>
         <Routes>
-          <Route exact path='/' element={<Register/>} />
+          <Route exact path='/' element={<Register />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
