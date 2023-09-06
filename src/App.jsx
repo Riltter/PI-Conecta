a20a88efdbd2f2f14c50b5ea309132a6b354fd98
Riltter <riltterkennedy@gmail.com>
2023-09-06 19:54:30 -0300

    formulario cadastro - front-end completo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import Register from './components/pages/Register';
 import Profile from './components/pages/Profile';
 import Feed from './components/pages/Feed';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 
 import './App.css'
+import Login from './components/pages/Login';
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path='/' element={<Register />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,48 +1,122 @@
 import style from './Register.module.css'
+import { Link } from "react-router-dom";
+import { useState } from 'react';
+
+const Register = () => {
+
+    const [nomeCompleto, setNomeCompleto] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirmSenha, setConfirmSenha] = useState("");
+    const [nomeUsuario, setNomeUsuario] = useState("");
 
 
-const formatarCPF = () => {
-    console.log("foi")
-};
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (senha === confirmSenha ? alert("Senhas conferem") : alert("Senhas não conferem"));
+    };
 
-function Register() {
     return (
         <>
             <section></section>
             <section></section>
             <section className={style.secao_formulario_cadastro}>
-                <p>Quer se conectar aos outros estudantes? Faça o seu cadastro</p>
+                <p className={style.titleRegister}>Quer se conectar aos outros estudantes? Faça o seu cadastro!</p>
                 <div className={style.formularioDeCadastro}>
-                    <form action="#" className={style.form_input_group}>
-                        <div className={style.input_box}>
-                            <label htmlFor="userName">Nome do usuário</label>
-                            <input type="text" id="userNameId" name="userName" placeholder='Digite o seu nome de usuário' required />
+                    <form onSubmit={handleSubmit}>
+                        <div className={style.form_input_group}>
+                            <div className={style.input_box}>
+                                <label htmlFor="name">Nome Completo</label>
+                                <input 
+                                    type="text" 
+                                    id="nameId" 
+                                    name="name" 
+                                    placeholder='Digite o seu nome completo' 
+                                    onChange={(e) => setNomeCompleto(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="email">Email</label>
+                                <input 
+                                    type="email" 
+                                    id="emailId" 
+                                    name="email" 
+                                    placeholder='Digite o seu email institucional' 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="birthday">Data de nascimento</label>
+                                <input 
+                                    type="date" 
+                                    id="birthdayId" 
+                                    name="birthday" 
+                                    placeholder='dd/mm/aaaa' 
+                                    onChange={(e) => setBirthday(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="cpf">CPF</label>
+                                <input 
+                                    type="text"
+                                    id="cpfId"
+                                    name="cpf" 
+                                    maxLength="11" 
+                                    placeholder="000.000.000-00" 
+                                    onChange={(e) => setCpf(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="password">Senha</label>
+                                <input 
+                                    type="password" 
+                                    id="passwordId" 
+                                    name="password" 
+                                    onChange={(e) => setSenha(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="passwordConfirm">Confirme sua senha</label>
+                                <input 
+                                    type="password" 
+                                    id="passwordConfirmId" 
+                                    name="passwordConfirm" 
+                                    onChange={(e) => setConfirmSenha(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
+                            <div className={style.input_box}>
+                                <label htmlFor="userName">Nome do usuário</label>
+                                <input 
+                                    type="text" 
+                                    id="userNameId" 
+                                    name="userName" 
+                                    placeholder='Digite o seu nome de usuário' 
+                                    onChange={(e) => setNomeUsuario(e.target.value)} 
+                                    required 
+                                    />
+                            </div>
                         </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="name">Nome Completo</label>
-                            <input type="text" id="nameId" name="name" placeholder='Digite o seu nome completo' required />
+                        <div>
+                            <input 
+                                className={style.btnCadastrar} 
+                                type="submit" 
+                                value={"Cadastrar"} 
+                                />
+                            <p className={style.pHaveAcconut}>Já tem uma conta? <Link to="/login">Clique aqui</Link></p>
+
                         </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="emailId" name="email" placeholder='Digite o seu email institucional' required />
-                        </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="birthday">Data de nascimento</label>
-                            <input type="date" id="birthdayId" name="birthday" placeholder='dd/mm/aaaa' required />
-                        </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="cpf">CPF</label>
-                            <input type="text" id="cpfId" name="cpf" maxLength="14" placeholder="000.000.000-00" onInput={formatarCPF} required />
-                        </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="password">Senha</label>
-                            <input type="password" id="passwordId" name="password" required />
-                        </div>
-                        <div className={style.input_box}>
-                            <label htmlFor="passwordConfirm">Confirme sua senha</label>
-                            <input type="password" id="passwordConfirmId" name="passwordConfirm" required />
-                        </div>
+
+
                     </form>
+
                 </div>
             </section>
         </>
