fd0ec35233222f38a51a317e78c2883737144871
miguellam <miguelabrantes12@gmail.com>
2023-09-16 13:59:18 -0300

    alterei a pagina de cadastro de projetos

--- src/components/pages/RegisterProject.jsx
+++ src/components/pages/RegisterProject.jsx
@@ -3,11 +3,67 @@ import { Link } from "react-router-dom";
 import { useState } from 'react';
 
 const RegisterProject = () => {
+    const [NomePorjeto, setNomeProjeto] = useState("");
+    const [AreaInteresse, setAreaInteresse] = useState("");
+    const [NumParticiapantes, setNumParticipantes] = useState("");
+    const [Descricao, setDescricao] = useState("");
 
     return (
         <>
-        <div>
-            
+        <div className={style.DivImgTelaCadastro}>
+            <img className={style.imgTelaCadastro} src=".\src\assets\cadastro projeto img.jpg" alt="ideia"/>
+            <form>
+                <h1 className={style.Titulo}>Cadastre seu Projeto!</h1> 
+                <div className={style.ImputGroup}>
+                    <div className={style.ImputBox}>
+                        <label htmlFor="nameProjeto">Nome do Projeto</label>
+                        <input type="text"
+                        name='nameProjeto'
+                        placeholder='Nome do Projeto'
+                        onChange={(e) => setNomeProjeto(e.target.value)}
+                        required
+                        />
+                    </div>
+
+                    <div className={style.ImputBox}>
+                        <label htmlFor="AreaInteresse">Área de Interesse</label>
+                        <input type="text" 
+                        name='AreaInteresse'
+                        placeholder='Ex. Enfermagem'
+                        onChange={(e) => setAreaInteresse(e.target.value)}
+                        required
+                        />
+                    </div>
+
+                    <div className={style.ImputBox}>
+                        <label htmlFor="NumParticipantes">Número de Participantes</label>
+                        <input type="number" 
+                        min="1"
+                        name='NumParticipantes'
+                        placeholder='Número de Participantes'
+                        onChange={(e) => setNumParticipantes(e.target.value)}
+                        required
+                        />
+                    </div>
+
+                    <div className={style.ImputBox}>
+                        <label htmlFor="Descricao">Descrição do Projeto</label>
+                        <input type="text" 
+                        name='Descricao'
+                        placeholder='Descreva seu projeto'
+                        onChange={(e) => setDescricao(e.target.value)}
+                        required
+                        />
+                    </div>
+
+                    <input 
+                        className={style.btnCadastrar} 
+                        type="submit" 
+                        value={"Cadastrar"} 
+                    />
+
+                </div>
+            </form>
         </div>
         </>
     );
