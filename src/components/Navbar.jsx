e7256cf40771c3cc59fe374e343ba0efdf13d64e
Thallyston <thallyston.schappo@gmail.com>
2023-10-04 17:24:27 -0300

    Terminando a NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,64 @@ import styled from "styled-components";
 import { BiSearch, BiSliderAlt, BiWorld, BiBell, BiMenu } from "react-icons/bi";
 import { RiUserSearchLine } from "react-icons/ri";
 
-
-// Elementos do CSS
-const Navegacao = styled.nav `
+const Navegacao = styled.nav`
     display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
-    height: 4.85rem;
+    height: 5rem;
     background-color: white;
     position: absolute;
-    pading botton: 10;
 `;
 
 const LogoConecta = styled.img`
-    
+    max-height: 100%;
+    margin-right: 20px;
 `;
 
 const BarraPesquisa = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    flex-grow: 0;
+    width: 900px;
+    margin-right: 20px;
+    position: relative;
+    background-color: white;
+`;
 
+const InputPesquisa = styled.input`
+    flex-grow: 1; /* Ocupar todo o espaço disponível */
+    font-size: 1.2rem;
+    padding: 10px;
+    border: none;
+    outline: none;
+`;
+
+const BotaoBarraPesquisa = styled.button`
+    font-size: 1.4rem;
+    padding: 10px;
+    background-color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
 `;
 
 const IconesNavegacao = styled.div`
-    
+    display: flex;
+    align-items: center;
 `;
 
-
+const BotaoIconesNavegacao = styled.button`
+    font-size: 2.4rem;
+    padding: 10px;
+    background-color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+`;
 
 const Navbar = () => {
     return (
@@ -33,18 +67,16 @@ const Navbar = () => {
             <Navegacao>
                 <LogoConecta src="./src/assets/Logo para Navbar.png" alt="Logo" />
                 <BarraPesquisa>
-                    <button type="button"><BiSearch /></button>
-                    <input type="text" placeholder="Pesquisar..." />
-                    <button type="button"><BiSliderAlt /></button>
+                    <BotaoBarraPesquisa type="button"><BiSearch /></BotaoBarraPesquisa>
+                    <InputPesquisa type="text" placeholder="Pesquisar..." />
+                    <BotaoBarraPesquisa type="button"><BiSliderAlt /></BotaoBarraPesquisa>
                 </BarraPesquisa>
                 <IconesNavegacao>
-                    <button type="button"><BiWorld /></button>
-                    <button type="button"><RiUserSearchLine /></button>
-                    <button type="button"><BiBell /></button>
-                    <button type="button"><BiMenu /></button>
+                    <BotaoIconesNavegacao type="button"><BiWorld /></BotaoIconesNavegacao>
+                    <BotaoIconesNavegacao type="button"><RiUserSearchLine /></BotaoIconesNavegacao>
+                    <BotaoIconesNavegacao type="button"><BiBell /></BotaoIconesNavegacao>
+                    <BotaoIconesNavegacao type="button"><BiMenu /></BotaoIconesNavegacao>
                 </IconesNavegacao>
-
-
             </Navegacao>
         </>
     )
