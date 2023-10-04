337c8c4172a582c8b27c328f192c597499df9d06
Ana <anabonjardimbarros@gmail.com>
2023-10-04 08:38:41 -0300

    imagem

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -10,40 +10,40 @@ const Login = () => {
 
     return (
         <>
-        <div className={style.DivImgTelaCadastro}>
-            <img className={style.imgTelaCadastro} src=".\src\assets\" alt="ideia"/>
-            <form className={style.FormProject}>
-                <h1 className={style.Titulo}>Oi de novo! <br/>Que bom que voltou</h1> 
-                <div className={style.ImputGroup}>
-                    <div className={style.ImputBox}>
-                        <label htmlFor="nameProjeto">Login</label>
-                        <input type="text"
-                        name='nameProjeto'
-                        placeholder='Email'
-                        onChange={(e) => setNomeProjeto(e.target.value)}
-                        required
-                        />
+            <div className={style.DivImgTelaCadastro}>
+                <img className={style.imgTelaCadastro} src=".\src\assets\maos.jpg" alt="uniao" />
+                <form className={style.FormProject}>
+                    <h1 className={style.Titulo}>Oi de novo! <br />Que bom que voltou!</h1>
+                    <div className={style.ImputGroup}>
+                        <div className={style.ImputBox}>
+                            <label htmlFor="nameProjeto">Login</label>
+                            <input type="text"
+                                name='nameProjeto'
+                                placeholder='Email'
+                                onChange={(e) => setNomeProjeto(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className={style.ImputBox}>
+                            <label htmlFor="AreaInteresse"></label>
+                            <input type="text"
+                                name='AreaInteresse'
+                                placeholder='senha'
+                                onChange={(e) => setAreaInteresse(e.target.value)}
+                                required
+                            />
+                        </div>
                     </div>
 
-                    <div className={style.ImputBox}>
-                        <label htmlFor="AreaInteresse"></label>
-                        <input type="text" 
-                        name='AreaInteresse'
-                        placeholder='senha'
-                        onChange={(e) => setAreaInteresse(e.target.value)}
-                        required
-                        />
-                    </div>
-                </div>
-
-                <input 
-                        className={style.btnCadastrar1} 
-                        type="submit" 
-                        value={"Cadastrar"} 
+                    <input
+                        className={style.btnCadastrar1}
+                        type="submit"
+                        value={"Cadastrar"}
                     />
 
-            </form>
-        </div>
+                </form>
+            </div>
         </>
     );
 
