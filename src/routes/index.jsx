aaeb5b15751437a0c6366bdbc0197b0b247455c0
Ana <anabonjardimbarros@gmail.com>
2023-10-20 19:13:08 -0300

    coloquei a rota do thata

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,21 +7,22 @@ import Login from "../components/pages/Login";
 import useAuth from "../hooks/useAuth";
 
 const Private = ({ item: Item }) => {
-    const { signed } = useAuth();
-    return signed ? <Item /> : <Login />;
-}
+  const { signed } = useAuth();
+  return signed ? <Item /> : <Login />;
+};
 
 const RoutesApp = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Register />} />
-                <Route path="/feed" element={<Private item={Feed} />} />
-                <Route path="/profile" element={<Private item={Profile} />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/feed" element={<Private item={Feed} />} />
+        <Route path="/profile" element={<Private item={Profile} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registerproject" element={<RegisterProject />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default RoutesApp;
