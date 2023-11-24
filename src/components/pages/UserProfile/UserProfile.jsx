c830f491856c14b6aa5a7a118afc6130f7087d73
Halycia de Oliveira <halyciadeoliveira@gmail.com>
2023-11-24 18:24:44 -0300

    alterações na tela user profile

--- src/components/pages/UserProfile/UserProfile.jsx
+++ src/components/pages/UserProfile/UserProfile.jsx
@@ -4,22 +4,30 @@ import { Link } from "react-router-dom";
 import { BiLogoLinkedinSquare, BiLogoInstagram } from "react-icons/bi";
 import { CgMail } from "react-icons/cg";
 import { RiGraduationCapLine } from "react-icons/ri";
+import { MdOutlineEdit } from "react-icons/md";
 
 
-const campus = 'Aqui ficará a lógica para mostrar a sessão "Campus"'
+const campus = 'Asa Norte'
 
-const sobreUserProfile = 'Aqui ficará a lógica para mostrar a sessão "Sobre"'
+const sobreUserProfile = ''
 
 const UserProfile = () => {
+    const pageStyle = {
+        backgroundColor: '#f5f5f5',
+        margin: 0,
+        padding: 0,
+        minHeight: '91.2vh',
+    };
+    {/*para deixar como componente, é pra criar um arquivo separado? */ }
 
     function downloadFile() {
-        var fileUrl = "../assets/Curriculo.jpg"; 
+        var fileUrl = "../assets/Curriculo.jpg";
         // ver como vai fazer pra puxar no banco o arquivo do curriculo
         var link = document.createElement('a');
-        
+
         link.href = fileUrl;
         link.download = 'Curriculo.jpg';
-        
+
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -28,42 +36,59 @@ const UserProfile = () => {
     return (
         <>
             <Navbar />
-            <div className={style.bodyUserProfile}>
-                <div className={style.coluna1UserProfile}>
-                    <Link to="/PaginaMeusProjetos">
-                        <button className={style.btnMeusProjetos} type="button">
-                            Meus projetos
-                        </button>
-                    </Link>
-                    <div className={style.linhaDeRedirecionamentoExterno}>
-                        <a href="/url linkedin ou api?">
-                            <button className={style.btnLinkedIn} type="button">
-                                <BiLogoLinkedinSquare />
+            <div style={pageStyle}>
+                <div className={style.bodyUserProfile}>
+                    <div className={style.capaUserProfile}>
+                        <div className={style.fotoDePerfil}>
+                            <img src="/assets/userProfile_fotoDePerfil.jpg" alt="Foto de Perfil" />
+                        </div>
+                        <div className={style.nomeDePerfil}>
+                            <p>Maria_Fernanda</p>
+                        </div>
+                        <Link to="/editprofile">
+                            <button className={style.btnEditProfile} type="button">
+                                <MdOutlineEdit />
                             </button>
-                        </a>
-                        <a href="/url linkedin ou api?">
-                            <button className={style.btnInstagram} type="button">
-                                <BiLogoInstagram />
-                            </button>
-                        </a>
-                        <a href="/url linkedin ou api?">
-                            <button className={style.btnGMail} type="button">
-                                <CgMail />
-                            </button>
-                        </a>
+                        </Link>
                     </div>
-                    <div className={style.Campus}>
-                        <RiGraduationCapLine />
-                        <p>{campus}</p>
+                    <div className={style.coluna1UserProfile}>
+                        <Link to="/PaginaMeusProjetos">
+                            <button className={style.btnMeusProjetos} type="button">
+                                Meus projetos
+                            </button>
+                        </Link>
+                        <div className={style.linhaDeRedirecionamentoExterno}>
+                            <a href="/url linkedin ou api?">
+                                <button className={style.btnLinkedIn} type="button">
+                                    <BiLogoLinkedinSquare />
+                                </button>
+                            </a>
+                            <a href="/url linkedin ou api?">
+                                <button className={style.btnInstagram} type="button">
+                                    <BiLogoInstagram />
+                                </button>
+                            </a>
+                            <a href="/url linkedin ou api?">
+                                <button className={style.btnGMail} type="button">
+                                    <CgMail />
+                                </button>
+                            </a>
+                        </div>
+                        <div className={style.Campus}>
+                            <RiGraduationCapLine />
+                            <p>{campus}</p>
+                        </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <label htmlFor="sobre">Sobre</label>
-                        <p>{sobreUserProfile}</p>
-                    </div>
+                        <div>
+                            <label htmlFor="sobre">Sobre</label>
+                            <div className={style.retanguloCinza}>
+                                <p>{sobreUserProfile}</p>
+                            </div>
+                        </div>
                         {/*Descobrir como colocar um pdf pra baixar no pc dos outros kkkk */}
                         <button className={style.btnUploadCurriculo} onClick={(e) => downloadFile()}>Download Arquivo</button>
+                    </div>
                 </div>
             </div>
         </>
